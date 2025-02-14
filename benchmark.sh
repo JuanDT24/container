a8a97047fb4f3a73ceb4c7679ef9241aff976a34
JuanDT24 <tiradodj@uninorte.edu.co>
2025-02-14 01:04:47 -0500

    Yes

--- benchmark.sh
+++ benchmark.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 git clone https://github.com/JuanDT24/Fibonacci.git tests
 
+echo "" > results.txt
 echo "Benchmark results" 
 echo "-----------------"
-for lang in tests/*; do
-    echo "$lang:"
-    echo "${lang}_benchmark"
-    docker build --no-cache -t "${lang}_benchmark" "$lang"
-    docker run --rm -v "$(pwd):/app" "${lang}_benchmark"
-    cat "$lang/output.txt"
-    echo "----------"
+for lang in c java js python php; do
+     lang_name=$(basename "$lang")
+        echo "Ejecutando $lang_name..."
+
+        docker build -t "${lang_name}_benchmark" "$lang"
+
+        TIME_OUTPUT=$(docker run --rm "${lang_name}_benchmark")
+
+        echo "$lang_name: $TIME_OUTPUT" >> results.txt
+        echo "#$TIME_OUTPUT"
 done
