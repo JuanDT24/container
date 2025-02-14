058b556c41c8fd1e17858f9cfb0c0e05fe048308
JuanDT24 <tiradodj@uninorte.edu.co>
2025-02-13 21:53:27 -0500

    Trying the benchmark script

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM docker:latest
 WORKDIR /app
 COPY . /app
 RUN apk add --no-cache bash git
+RUN apk add --no-cache alpine-base
 CMD ["sh", "./benchmark.sh"]
---
+++ benchmark.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+git clone https://github.com/JuanDT24/container.git tests
+
+echo "Benchmark results" 
+echo "-----------------"
+for lang in tests/*; do
+    echo "$lang:"
+    cat "./$lang/${lang}_output.txt"
+    echo "-----
